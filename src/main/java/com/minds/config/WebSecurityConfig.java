36e947928b9062722d7b8daaab20d10fdf0c8f40
Javier Abad <jabad@openkm.com>
2018-04-12 16:25:09 +0200

    Commit

--- src/main/java/com/minds/config/WebSecurityConfig.java
+++ src/main/java/com/minds/config/WebSecurityConfig.java
@@ -27,36 +27,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/example/**").authenticated().antMatchers(Constants.LOGIN_URL).permitAll().and()
 				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
-				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
-		// /*
-		// * 1. Se desactiva el uso de cookies 2. Se activa la configuración CORS con
-		// los
-		// * valores por defecto 3. Se desactiva el filtro CSRF 4. Se indica que el
-		// login
-		// * no requiere autenticación 5. Se indica que el resto de URLs esten
-		// securizadas
-		// */
-		// httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-		// .csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST,
-		// Constants.LOGIN_URL).permitAll()
-		// .anyRequest().authenticated().and().addFilter(new
-		// JWTAuthenticationFilter(authenticationManager()))
-		// .addFilter(new JWTAuthorizationFilter(authenticationManager()));
+				.addFilter(new JWTAuthorizationFilter(authenticationManager()));		
 	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// Se define la clase que recupera los usuarios y el algoritmo para procesar las
-		// passwords
 		auth.userDetailsService(authService).passwordEncoder(bCryptPasswordEncoder());
 	}
-
-	// @Bean
-	// CorsConfigurationSource corsConfigurationSource() {
-	// final UrlBasedCorsConfigurationSource source = new
-	// UrlBasedCorsConfigurationSource();
-	// source.registerCorsConfiguration("/**", new
-	// CorsConfiguration().applyPermitDefaultValues());
-	// return source;
-	// }
+	
 }
